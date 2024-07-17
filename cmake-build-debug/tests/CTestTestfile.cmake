81d6156166427a838619105b3e7d84e59013d7ff
anthonymakarewicz <138609270+anthonymakarewicz@users.noreply.github.com>
2024-07-17 16:50:47 +0200

    add .gitignore and removed all build and cmake directories from git

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anthony/CLionProjects/Option_pricer/tests
-# Build directory: /Users/anthony/CLionProjects/Option_pricer/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("unit")
-subdirs("integration")
