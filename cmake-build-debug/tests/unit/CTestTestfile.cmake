d30b89238d92d4cebe1767ce6fb197247c6acb05
anthonymakarewicz <138609270+anthonymakarewicz@users.noreply.github.com>
2024-07-17 14:34:50 +0200

    forgot to commit the cmake-build-debug

---
+++ cmake-build-debug/tests/unit/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anthony/CLionProjects/Option_pricer/tests/unit
+# Build directory: /Users/anthony/CLionProjects/Option_pricer/cmake-build-debug/tests/unit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[UnitTests]=] "/Users/anthony/CLionProjects/Option_pricer/cmake-build-debug/tests/unit/Unit_tests")
+set_tests_properties([=[UnitTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/anthony/CLionProjects/Option_pricer/tests/unit/CMakeLists.txt;19;add_test;/Users/anthony/CLionProjects/Option_pricer/tests/unit/CMakeLists.txt;0;")
