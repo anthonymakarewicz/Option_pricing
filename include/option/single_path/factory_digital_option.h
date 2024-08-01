7dcf7ad34b52558da1528c33367511c4943b6d8e
anthonymakarewicz <138609270+anthonymakarewicz@users.noreply.github.com>
2024-08-01 12:54:22 +0200

    added factory for digital

--- include/option/single_path/factory_digital_option.h
+++ include/option/single_path/factory_digital_option.h
@@ -1,12 +1,12 @@
-#ifndef FACTORY_EUROPEAN_OPTION_H
-#define FACTORY_EUROPEAN_OPTION_H
+#ifndef FACTORY_DIGITAL_OPTION_H
+#define FACTORY_DIGITAL_OPTION_H
 
 #include "option/single_path/factory_single_path_option.h"
 
 namespace OptionPricer {
-    class EuropeanOptionFactory final: public SinglePathOptionFactory {
+    class DigitalOptionFactory final: public SinglePathOptionFactory {
         std::shared_ptr<SinglePathOption> createOption(const ParameterObject& params, const std::string& type) override;
     };
 }
 
-#endif //FACTORY_EUROPEAN_OPTION_H
+#endif //FACTORY_DIGITAL_OPTION_H
--- src/option/single_path/factory_digital_option.cpp
+++ src/option/single_path/factory_digital_option.cpp
@@ -1,26 +1,26 @@
-#include "option/single_path/factory_european_option.h"
+#include "option/single_path/factory_digital_option.h"
 
 #include <market_data/market_data.h>
-#include "option/single_path/european_option.h"
-#include "payoff/single_strike/factory_payoff_vanilla.h"
+#include "option/single_path/digital_option.h"
+#include "payoff/single_strike/factory_payoff_digital.h"
 
 namespace OptionPricer {
-    std::shared_ptr<SinglePathOption> EuropeanOptionFactory::createOption(const ParameterObject &params,
+    std::shared_ptr<SinglePathOption> DigitalOptionFactory::createOption(const ParameterObject &params,
             const std::string &type) {
         auto marketData = MarketData::getInstance();
         std::shared_ptr<SinglePathOption> eurOption = nullptr;
 
         try {
-            PayoffVanillaFactory payoffFactory;
+            PayoffDigitalFactory payoffFactory;
             auto payoff = payoffFactory.createPayoff(type, params.getParameter<double>("K"));
-            eurOption = std::shared_ptr<SinglePathOption>(new EuropeanOption(
+            eurOption = std::shared_ptr<SinglePathOption>(new DigitalOption(
                 params.getParameter<std::string>("ticker"),
                 std::move(payoff),
                 params.getParameter<double>("T"),
                 marketData
                 ));
         } catch (const std::invalid_argument& e) {
-            const std::string err = std::string(e.what()) + "\n" + invalidParams("European Option");
+            const std::string err = std::string(e.what()) + "\n" + invalidParams("Digital Option");
             throw std::invalid_argument(err); // Re-throw the caught exception
         }
 
