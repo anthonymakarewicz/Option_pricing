e8e9b17fd2e2c081a96cd54e5987bd37c493bcfa
anthonymakarewicz <138609270+anthonymakarewicz@users.noreply.github.com>
2024-08-10 14:49:12 +0200

    deleted build directories from git

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anthony/CLionProjects/Option_pricer
-# Build directory: /Users/anthony/CLionProjects/Option_pricer/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("examples")
